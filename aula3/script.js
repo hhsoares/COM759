1046e68e35af1e8cd46d5d6fe5c9fbfee5562d6a
Hugo Soares <hhsoares112@gmail.com>
2025-03-18 20:07:02 -0300

    added all checks

--- aula3/script.js
+++ aula3/script.js
@@ -6,23 +6,46 @@ function validateForm() {
   let email = document.getElementById("email").value;
   let senha = document.getElementById("senha").value;
 
+  let ra_ano = ra.slice(0, 4);
+
   console.log(ra, nome, email, senha);
+  console.log(ra_ano);
 
   console.log(`${ra.length}`);
   console.log(`${senha.length}`);
 
   if ( ra === "" || nome === "" || email === "" || senha === "" ) {
-    console.log("Preencha todos os campos!");
+    alert("Preencha todos os campos!");
     return false;
   }
 
   if (senha.length < 10) {
-    console.log("A senha deve ter pelo menos 10 caracteres!");
+    alert("A senha deve ter pelo menos 10 caracteres!");
     return false;
   }
 
   if (ra.length != 9) {
-    console.log("O RA deve ter 9 caracteres!");
+    alert("O RA deve ter 9 caracteres!");
+    return false;
+  }
+
+  if (ra_ano < 1999 || ra_ano > 2025) {
+    alert("Ano do RA invalido!")
+    return false
+  }
+
+  if (!/^\d+$/.test(ra)) {
+    alert("O RA deve conter apenas números!")
+    return false;
+  }
+
+  if (!/@/.test(email)) {
+    alert("E-mail inválido!")
+    return false;
+  }
+
+  if (!/ /.test(nome)) {
+    alert("Digite o nome e sobrenome!")
     return false;
   }
 
